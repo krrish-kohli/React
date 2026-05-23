e29bab10cb6fd17b191a116e312e17c3e725a1f4
krrish-kohli <krrishkohli15@gmail.com>
2026-05-23 15:14:40 -0700

    new files added

---
+++ sideeffect-demo/src/App.jsx
@@ -0,0 +1,17 @@
+import React, { useEffect, useState } from "react";
+
+const App = () => {
+  const [count, setCount] = useState(0);
+  useEffect(() => {
+    // Side effect code wil be here
+    console.log("This runs after render");
+  }, [count]); // we can have no list which means it runs after every render (not suggested); we can also have an empty list which means it runs the first time the component appears on the page; we can also pass in array with some values
+  return (
+    <div>
+      <h1>Counter: {count}</h1>
+      <button onClick={() => setCount(count + 1)}>Increase</button>
+    </div>
+  );
+};
+
+export default App;
