eef41a0068be64d4f275c27eff269babc7e3a23b
krrish-kohli <krrishkohli15@gmail.com>
2026-05-23 17:38:18 -0700

    new files added

---
+++ routing-basics/src/components/About.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div>
+      <h2>About Page</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Libero eos
+        unde, quam ex officia voluptate ad! Illo iusto veniam dolore! Odio nobis
+        praesentium ab. Corrupti adipisci accusamus consectetur sed pariatur.
+      </p>
+    </div>
+  );
+};
+
+export default About;
